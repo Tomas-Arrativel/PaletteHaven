927edc0d93a6b6f8dabfd39d70b5550f860491ad
Tomás Arrativel <arrativeltomas@gmail.com>
2023-11-24 11:44:31 -0300

    initializating the design of the banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { Header, Footer } from './containers/exports';
 
 function App() {
--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
 import styles from './Banner.module.css';
 
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <div className={styles.banner}>
+      <div className={styles.text__container}>
+        <h1>PaletteHaven</h1>
+        <p>
+          Elevate your online presence with PaletteHaven – where website
+          solutions meet artistic precision.
+        </p>
+        <a className={styles.contact__btn}>Contact</a>
+      </div>
+      <div className={styles.img__container}>
+        <img
+          className={styles.image}
+          src='./banner/latas-pintura.png'
+          alt='image of paint'
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
