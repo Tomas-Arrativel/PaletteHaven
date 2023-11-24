6415a39a291504fdf84372dae2601bc3185c1962
Tomás Arrativel <arrativeltomas@gmail.com>
2023-11-24 09:29:27 -0300

    responsive navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,35 @@
 import styles from './Navbar.module.css';
+import { HiMenuAlt3 } from 'react-icons/hi';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  const onClick = () => {
+    setActive(!active);
+  };
   return (
     <nav className={styles.navbar}>
       <h2 className={styles.title}>PaletteHaven</h2>
       <ul className={styles.list}>
         <li className={styles.list__item}>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+        <li className={styles.list__item}>About</li>
+        <li className={styles.list__item}>Contact</li>
       </ul>
+
+      <div className={styles.res__list}>
+        <div className={styles.icon}>
+          <HiMenuAlt3 onClick={onClick} />
+        </div>
+        <ul
+          className={styles.res__menu}
+          style={active ? { right: 0 } : { right: '-71%' }}
+        >
+          <li className={styles.list__item}>Home</li>
+          <li className={styles.list__item}>About</li>
+          <li className={styles.list__item}>Contact</li>
+        </ul>
+      </div>
     </nav>
   );
 };
