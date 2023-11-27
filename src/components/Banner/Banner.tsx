618cf48149b6c36f29da9c77a8ca5a6ba7baac33
Tomás Arrativel <arrativeltomas@gmail.com>
2023-11-27 09:27:07 -0300

    Adding media queries to the banner

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -2,14 +2,35 @@ import styles from './Banner.module.css';
 
 const Banner = () => {
   return (
-    <div className={styles.banner}>
+    <div id='banner' className={styles.banner}>
       <div className={styles.text__container}>
         <h1>PaletteHaven</h1>
         <p>
           Elevate your online presence with PaletteHaven – where website
           solutions meet artistic precision.
         </p>
-        <a className={styles.contact__btn}>Contact</a>
+        <div className={styles.benefits}>
+          <h2>Our websites can:</h2>
+          <ul>
+            <li>
+              ✔ Generate more and{' '}
+              <span className={styles.bold}>better leads</span>,
+            </li>
+            <li>
+              ✔ Build <span className={styles.bold}>trust</span>
+            </li>
+            <li>
+              ✔ and Book more <span className={styles.bold}>calls</span>
+            </li>
+          </ul>
+        </div>
+        <p className={styles.cta__p}>
+          If the proposition appears favorable, <br />
+          <span>Why not consider giving it a try? ⬇</span>
+        </p>
+        <a href='#contact' className={styles.contact__btn}>
+          Contact
+        </a>
       </div>
       <div className={styles.img__container}>
         <img
