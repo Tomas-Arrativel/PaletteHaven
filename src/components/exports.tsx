69d7a8625d3e789f151b3dc3fdde6e37289d9de3
Tomás Arrativel <arrativeltomas@gmail.com>
2023-11-28 11:21:10 -0300

    adding global class bold

--- src/components/exports.tsx
+++ src/components/exports.tsx
@@ -2,3 +2,4 @@ export { default as Service } from './Service/Service';
 export { default as Navbar } from './Navbar/Navbar';
 export { default as Banner } from './Banner/Banner';
 export { default as Blueprint } from './Blueprint/Blueprint';
+export { default as LikeYou } from './LikeYou/LikeYou';
