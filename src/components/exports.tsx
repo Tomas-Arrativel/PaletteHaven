a3f2b4e9aa4221b8a4b8a4d5b5a8948a09122502
Tomás Arrativel <arrativeltomas@gmail.com>
2023-11-29 09:08:47 -0300

    export for More component

--- src/components/exports.tsx
+++ src/components/exports.tsx
@@ -3,3 +3,4 @@ export { default as Navbar } from './Navbar/Navbar';
 export { default as Banner } from './Banner/Banner';
 export { default as Blueprint } from './Blueprint/Blueprint';
 export { default as LikeYou } from './LikeYou/LikeYou';
+export { default as More } from './More/More';
