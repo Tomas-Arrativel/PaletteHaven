3571364304e710695f12d72ae0e0edba11923428
Tomás Arrativel <arrativeltomas@gmail.com>
2023-11-30 08:55:49 -0300

    Adding functionality to questions

--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -1,15 +1,28 @@
 import styles from './Question.module.css';
+import { Collapse } from 'react-collapse';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
 interface QuestionProps {
   question: string;
   content: string;
+  open: boolean | null;
+  toggle: any;
 }
 
-const Question = ({ question, content }: QuestionProps) => {
+const Question = ({ question, content, open, toggle }: QuestionProps) => {
   return (
     <section className={styles.question}>
-      <h2>{question}</h2>
-      <p>{content}</p>
+      <div className={styles.titleContainer} onClick={toggle}>
+        <h2>{question}</h2>
+        <div className={styles.icon}>
+          {open ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </div>
+      </div>
+      <Collapse isOpened={open}>
+        <div className={styles.description}>
+          <p>{content}</p>
+        </div>
+      </Collapse>
     </section>
   );
 };
