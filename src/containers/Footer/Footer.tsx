167d4c6f75c2df8d4ce94765d2cde7092d7db87b
Tomás Arrativel <arrativeltomas@gmail.com>
2023-12-04 09:12:50 -0300

    Footer structure and styling finished

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
 import styles from './Footer.module.css';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <p>
+        Copyright © PaletteHeaven 2023. All Rights Reserved. Unlawful
+        reproduction or use of content without permission is strictly
+        prohibited. Crafted with passion by PaletteHeaven.
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
